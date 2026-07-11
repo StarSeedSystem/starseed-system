8124f309e90e3320d82b6d2e19fdad88cb52742c
Alex Bordón Garrigós <alexbordongarrigos@gmail.com>
2026-07-11 12:29:37 -0600

    Fix: Move use client directive to top of file in profile-header.tsx to fix build

--- src/components/profile/profile-header.tsx
+++ src/components/profile/profile-header.tsx
@@ -1,6 +1,6 @@
-import Link from "next/link";
-
 "use client";
+
+import Link from "next/link";
 import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
